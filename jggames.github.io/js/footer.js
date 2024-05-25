d8e9eff6e6ff26d4c6208ba4195dc2da01dcefbc
Jim Gerrie <jimgerrie@ns.sympatico.ca>
2024-05-25 11:09:52 -0300

    Jim Gerrie on Github

---
+++ jggames.github.io/js/footer.js
@@ -0,0 +1,4 @@
+	document.write('<p align="center">Send a comment to ');
+	document.write('<a id="name="SendToJim" name="SendToJim" href="mailto:jimgerrie@ns.sympatico.ca">James Gerrie</a> or ');
+	document.write('<a id="name="SendToPatty" name="SendToPatty" href="mailto:pattygerrie@ns.sympatico.ca">Patricia Gerrie</a><br>');
+	document.write('Go to <a id="name="homepage" name="homepage" href="http://www3.ns.sympatico.ca/jimgerrie/index.html">James Gerrie&#146;s Home Page</a></p>');
